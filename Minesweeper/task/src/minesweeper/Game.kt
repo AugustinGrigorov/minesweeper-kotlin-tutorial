8fe4af021e06c52fcd247774c7a3937401c10205
Augustin Grigorov <grigoroa@amazon.com>
2020-09-27 18:36:12 +0100

    Make game interactive

--- Minesweeper/task/src/minesweeper/Game.kt
+++ Minesweeper/task/src/minesweeper/Game.kt
@@ -3,42 +3,46 @@ package minesweeper
 import kotlin.random.Random
 
 class Game(private val boardSize: Int, minesRequired: Int) {
-    private val board: Array<Array<Int>> = Array(boardSize) { Array(boardSize) { 0 } }
+    private var mines: HashMap<List<Int>, Boolean> = hashMapOf()
+    private var numbers: HashMap<List<Int>, Boolean> = hashMapOf()
+    private var marked: HashMap<List<Int>, Boolean> = hashMapOf()
 
     init {
         if (minesRequired > boardSize * boardSize) {
             throw Exception("Can't fit this many mines on the board")
         }
         for (i in 0 until minesRequired) {
-            var randomHeight = Random.nextInt(0, boardSize)
-            var randomWidth = Random.nextInt(0, boardSize)
-            while (board[randomHeight][randomWidth] == 1) {
-                randomHeight = Random.nextInt(0, boardSize)
-                randomWidth = Random.nextInt(0, boardSize)
+            var coordinate = listOf(Random.nextInt(0, boardSize), Random.nextInt(0, boardSize))
+            while (coordinate in mines) {
+                coordinate = listOf(Random.nextInt(0, boardSize), Random.nextInt(0, boardSize))
             }
-            board[randomHeight][randomWidth] = 1
+            mines[coordinate] = true
         }
     }
 
     fun render(): String {
-        var result = ""
+        var result = " │123456789│\n—│—————————│\n"
         for (i in 0 until boardSize) {
+            result += "${i + 1}|"
             for (j in 0 until boardSize) {
-                result += when {
-                    board[i][j] == 1 -> "X"
-                    board[i][j] == 0 -> {
+                var coordinate = listOf(i, j)
+                result += when (coordinate) {
+                    in marked -> "*"
+                    in mines -> "."
+                    else -> {
                         val surroundingMines = getSurroundingMines(i, j)
                         if(surroundingMines == 0) {
                             "."
                         } else {
+                            numbers[coordinate] = true
                             surroundingMines.toString()
                         }
                     }
-                    else -> throw Exception("Unsupported value in grid")
                 }
             }
-            result += "\n"
+            result += "|\n"
         }
+        result += "—│—————————│\n"
         return result
     }
 
@@ -46,9 +50,26 @@ class Game(private val boardSize: Int, minesRequired: Int) {
         var mineCount = 0
         for (i in maxOf(0, height - 1) until minOf(boardSize, height + 2)) {
             for (j in maxOf(0, width - 1) until minOf(boardSize, width + 2)) {
-                if (board[i][j] == 1) mineCount++
+                var coordinate = listOf(i, j)
+                if (coordinate in mines) mineCount++
             }
         }
         return mineCount
+    }
+
+    fun markCoordinate(coordinate: List<Int>): Boolean {
+        if (coordinate in numbers) {
+            return false
+        }
+        if (coordinate in marked) {
+            marked.remove(coordinate)
+        } else {
+            marked[coordinate] = true;
+        }
+        return true
+    }
+
+    fun won(): Boolean {
+        return mines == marked
     }
 }
--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -5,4 +5,14 @@ fun main() {
     val minesRequired = readLine()!!.toInt()
     val game = Game(9, minesRequired)
     print(game.render())
+    while (!game.won()) {
+        println("Set/delete mine marks (x and y coordinates):")
+        val coordinates = readLine()!!.trim().split(" ").reversed().map { it.toInt() - 1 }
+        if (game.markCoordinate(coordinates)) {
+            print(game.render())
+        } else {
+            println("There is a number here!")
+        }
+    }
+    println("Congratulations! You found all the mines!")
 }
